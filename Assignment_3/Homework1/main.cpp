04df6e7860c21291b85f63fc84ef40c40fe82c82
ChenMinnie <410321101@gms.ndhu.edu.tw>
2018-01-23 16:32:34 +0800

    Solution 1

--- Assignment_3/Homework1/main.cpp
+++ Assignment_3/Homework1/main.cpp
@@ -25,30 +25,34 @@ int main() {
 
 	image_R = cvLoadImage(FReference, CV_LOAD_IMAGE_UNCHANGED); 
 	image_C = cvLoadImage(FTarget, CV_LOAD_IMAGE_UNCHANGED); 
-															 //if (!image) {
-															 //	cout << "Error: Couldn't open the image file.\n";
-															 //}
-								 //	Mat mat2 = cvarrToMat(image_R);
+	//if (!image) {
+			//	cout << "Error: Couldn't open the image file.\n";
+	//}
+	//	Mat mat2 = cvarrToMat(image_R);
+
 	Mat MatC = cvarrToMat(image_C);
 	Mat MatR = cvarrToMat(image_R);
-	Mat ip2 = cvarrToMat(image_R);
+
+	Mat ip2 (MatR.rows, MatR.cols, CV_8U);
+
 	int u, v;
+
 
 	int y0 = N / 2; int x0 = N / 2; 
 
-	for (x0 = N / 2; x0 < image_C->width; x0 = x0 + N) {
+	for (x0 = N / 2; x0 < image_C->height; x0 = x0 + N) {
 		
-		for (y0 = N / 2; y0 < image_C->height; y0 = y0 + N) {
- 
+		for (y0 = N / 2; y0 < image_C->width; y0 = y0 + N) {
+	
 		    Sequential(image_C, image_R, ip2, x0, y0);
 	
 		}
 	}
 	
-	//Sequential(image_C, image_R, x,y);
+
 	namedWindow("Display window", WINDOW_AUTOSIZE);
 	imshow("Display window", ip2);
-
+	imwrite("output.jpg", ip2);
 	cvShowImage("Reference", image_R); 
 	cvShowImage("Target", image_C);
 	cvWaitKey(0);		
@@ -65,7 +69,7 @@ double MAD(IplImage * C, IplImage *R, int i, int j,int X0,int Y0) {
 
 	Mat matC = cvarrToMat(C);
 	Mat matR = cvarrToMat(R);
-	
+
 	double a = 1 / (N * N);
 	double mad = 0;
 	double add = 0;
@@ -73,25 +77,23 @@ double MAD(IplImage * C, IplImage *R, int i, int j,int X0,int Y0) {
 	for (int k = 0; k < N - 1; k++) {
 		for (int l = 0; l < N - 1; l++) {
 
-			int nC = x + k + P;
-			int mC = y + l + P;
+			int xC = x + k + P;
+			int yC = y + l + P;
 
-			if (nC >= matC.rows - 1) nC = matC.rows - 1;
-			if (mC >= matC.cols - 1) mC = matC.cols - 1;
-			
-			//printf("x%d\n", nC);
-			//printf("y%d\n", mC);
-
-			double Cvalue =  matC.at<uchar>(nC, mC);
-
-			int nR = x + i + k + P;
-			int mR = y + j + l + P;
-
-			if (nR >= matC.rows - 1) nR = matC.rows - 1;
-			if( mR >= matC.cols - 1) mR = matC.cols - 1;
+			if (xC >= matC.rows - 1) xC = matC.rows - 1;
+			if (yC >= matC.cols - 1) yC = matC.cols - 1;
 			
 
-			double Rvalue = matR.at<uchar>(nR, mR);
+			double Cvalue =  matC.at<uchar>(xC, yC);
+
+			int xR = x + i + k + P;
+			int yR = y + j + l + P;
+
+			if (xR >= matC.rows - 1) xR = matC.rows - 1;
+			if( yR >= matC.cols - 1) yR = matC.cols - 1;
+			
+
+			double Rvalue = matR.at<uchar>(xR, yR);
 
 			add += abs(Cvalue - Rvalue);
 		}
@@ -103,11 +105,12 @@ double MAD(IplImage * C, IplImage *R, int i, int j,int X0,int Y0) {
 
 void Sequential(IplImage * C, IplImage *R, Mat ip2,int x0,int y0) {
 
-	
 	int u=0, v = 0;
-	//Mat matC = cvarrToMat(C);
+	
 	Mat matR = cvarrToMat(R);
+	
 	Mat dst(matR.rows, matR.cols, CV_8U);
+	
 	double cur_MAD = 0;
 	double min_MAD = 10000.0;
 
@@ -124,10 +127,33 @@ void Sequential(IplImage * C, IplImage *R, Mat ip2,int x0,int y0) {
 			}
 		}
 	}
+	int x = x0 - (N / 2.0), y = y0 - (N / 2.0);
 	
+	for (int q = 0; q < N; q++) {
+		for (int p = 0 ; p < N; p++) {
+			if (x >= ip2.rows - 1) x = ip2.rows -1;
+			
+			if (y >= ip2.cols - 1) y = ip2.cols -1;
 
+			int n = x + u + P;
 
+			int m = y + v + P;
 
+			if (n >= ip2.rows - 1) n = ip2.rows - 1;
+
+			if (m >= ip2.cols - 1) m = ip2.cols - 1;
+
+			ip2.at<uchar>(x, y) = matR.at<uchar>(n, m);
+		
+		
+		
+			 y++;
+		}
+		x++; 
+		y = y0 - (N / 2.0);
+	}
+
+	
 
 
 
